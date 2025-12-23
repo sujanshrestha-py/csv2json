f96c5e16032b409794eefb825e959d04d141da43
sujanshrestha-py <sujanshrestha.py@gmail.com>
2025-12-23 09:58:13 +0545

    Setup UI for CSV → JSON converter; logic to be implemented

---
+++ index.js
@@ -0,0 +1,8 @@
+let csv = document.getElementById('csv');
+let json = document.getElementById('json');
+let convertToJson = document.getElementById('convertToJson');
+let msg = document.getElementById('msg');
+
+convertToJson.addEventListener('click', () => {
+    
+});
