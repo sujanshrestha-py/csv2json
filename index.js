205f938a6812aec5c9b6786434892acccdd2a38f
sujanshrestha-py <sujanshrestha.py@gmail.com>
2025-12-25 08:56:04 +0545

    Implemented the logic as well.

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ let csv = document.getElementById('csv');
 let json = document.getElementById('json');
 let convertToJson = document.getElementById('convertToJson');
 let msg = document.getElementById('msg');
+let result = [];
+let headers = [];
+let current_line;
 
 convertToJson.addEventListener('click', () => {
+
+    if((csv.value).trim() == '') {
+        msg.style = 'display: block; color: red; font-size: 18px;';
+        msg.innerHTML = 'Please enter/paste csv content.';
+    }else {
+
+        rowData = (csv.value.trim()).split("\n");
+        headers = rowData[0].split(",");
+
+        for(i=1; i<rowData.length; i++) {
+            current_line = rowData[i].split(",");
+            let obj = {};
+            for(j=0; j<headers.length; j++) {
+                obj[headers[j]] = current_line[j];
+            }
+            result.push(obj)
+        }
+
+        json.innerHTML = JSON.stringify(result, null, 2);
+
+    }
     
 });
